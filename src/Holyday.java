fb35726dada91b7c094e4bc677e4cc0be1b6c090
Larissa Paixao <laryssapaixao9@gmail.com>
2023-01-13 18:08:30 -0300

    feat: change function getHolyday

---
+++ src/Holyday.java
@@ -0,0 +1,10 @@
+import java.util.HashMap;
+import java.util.Map;
+public class Holyday {
+
+    Map<String, String> Holyday = new HashMap<>();
+
+    public void getHolyday(String date){
+
+    }
+}
